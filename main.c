87cd6902d1030696d61a11832dc823f919ccc986
scz <st.comblez@gmail.com>
2025-11-10 08:56:36 +0100

    labo 4.1

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void asciiToChar();
+void inverseWord();
+
 int main(void) {
-    printf("Hello, World!\n");
+asciiToChar();
     return 0;
+}
+// Labo 1.4
+void asciiToChar () {
+    printf("Ecrire un caractère :\n");
+    int c =getchar();
+    printf("Le code ASCII de ce caractère est : %d\n",c);
+}
+// Labo 1.5
+void inverseWord () {
+    char word[20];
+     fgets(word,20,stdin);
 }
