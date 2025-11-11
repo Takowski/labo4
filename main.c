f2f64861de60152eb5047975127d51a180cff777
Takowski <93830775+Takowski@users.noreply.github.com>
2025-11-11 21:24:11 +0100

    Add code inspection severity options and implement palindrome function

--- main.c
+++ main.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 
 void asciiToChar();
+
 void inverseWord();
 
+void palindrome();
+
 int main(void) {
-asciiToChar();
+    // asciiToChar();
+    // inverseWord();
+    palindrome();
     return 0;
 }
-// Labo 1.4
-void asciiToChar () {
+
+// Labo 4.1
+void asciiToChar() {
     printf("Ecrire un caractère :\n");
-    int c =getchar();
-    printf("Le code ASCII de ce caractère est : %d\n",c);
+    int c = getchar();
+    printf("Le code ASCII de ce caractère est : %d\n", c);
 }
-// Labo 1.5
-void inverseWord () {
+
+// Labo 4.2
+void inverseWord() {
     char word[20];
-     fgets(word,20,stdin);
+    fgets(word, 20,stdin);
+    printf("%s", word);
 }
+
+// Labo 4.3
+
+void palindrome() {
+    int strl = 0;
+    char word[30];
+    int is_palindrome = 1;
+printf("ecrire un mot pour verifier si c'est un palindrome :\n");
+    fgets(word, 30,stdin);
+
+    while (word[strl] != '\0' && word[strl] != '\n') {
+        strl++;
+    }
+    for (int i = 0, j = strl-1;  i < j ; i++, j--) {
+        if (word[i] != word[j]) {
+            is_palindrome = 0;
+            break;
+        }
+    }
+    if (is_palindrome)
+        printf("c'est un palindrome");
+}
+
+// Labo 4.4
+
