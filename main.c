3b12a390788a5e58f5071abf8cf66f8956f4927b
Takowski <93830775+Takowski@users.noreply.github.com>
2025-11-12 08:37:14 +0100

    Refactor palindrome function to return a boolean value and update inverseWord function for consistency

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int asciiToChar();
 
-char* inverseWord();
+char *inverseWord();
 
-void palindrome();
+bool palindrome();
 
 int main(void) {
-    char *reverse=inverseWord();
+    // char *reverse = inverseWord();
     // printf("Le code ASCII de ce caractère est :%d \n", asciiToChar());
-    printf(" le mot inversé est : %s \n",reverse);
-    // palindrome();
-    free(reverse);
+    // printf(" le mot inversé est : %s \n", reverse);
+    printf("%s", palindrome() ? "est un palindrome" : "n'est pas un palindrome");
+    // free(reverse);
     return 0;
 }
 
@@ -25,45 +26,44 @@ int asciiToChar() {
 }
 
 // Labo 4.2
-char* inverseWord() {
+char *inverseWord() {
     char word[20];
     printf("Introduire un mot:\n");
     fgets(word, 20,stdin);
-    size_t len=strlen(word);
+    size_t len = strlen(word);
+
     if (len > 0 && word[len - 1] == '\n') {
         word[len - 1] = '\0';
         len--;
     }
-    char *reverse=malloc(sizeof(char)*(len+1));
-    for (int i=0 ,j=len-1;i<len;i++,j--) {
-        reverse[i]=word[j];
+    char *reverse = malloc(sizeof(char) * (len + 1));
+    for (int i = 0, j = len - 1; i < len; i++, j--) {
+        reverse[i] = word[j];
     }
-    reverse[len]='\0';
+    reverse[len] = '\0';
     return reverse;
-
 }
 
 // Labo 4.3
 
-void palindrome() {
+bool palindrome() {
     int len = 0;
     char word[30];
-    int is_palindrome = 1;
-printf("ecrire un mot pour verifier si c'est un palindrome :\n");
+    bool is_palindrome = true;
+    printf("ecrire un mot pour verifier si c'est un palindrome :\n");
     fgets(word, 30,stdin);
 
     while (word[len] != '\0' && word[len] != '\n') {
         len++;
     }
-    for (int i = 0, j = len-1;  i < j ; i++, j--) {
+    for (int i = 0, j = len - 1; i < j; i++, j--) {
         if (word[i] != word[j]) {
-            is_palindrome = 0;
+            is_palindrome = false;
             break;
         }
     }
-    if (is_palindrome)
-        printf("c'est un palindrome");
+
+    return is_palindrome;
 }
 
 // Labo 4.4
-
